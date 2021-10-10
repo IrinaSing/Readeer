1f22d0f533318e166370e72c6ef13032f6361b87
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-10-10 15:36:50 +0200

    user routes

--- api/routes/user.js
+++ api/routes/user.js
@@ -8,10 +8,19 @@ userRoutes.use((req, res, next) => {
   next();
 });
 
-// channelRoutes.get('/', channelController.get);
-// channelRoutes.get('/:channelId', channelController.getChannelById);
-// channelRoutes.delete('/:channelId', channelController.delete);
-// channelRoutes.put('/:channelId', channelController.put);
-// channelRoutes.post('/', channelController.post);
+// userRoutes.get('/', usersController.get);
+// userRoutes.get('/:channelId', usersController.getChannelById);
+userRoutes.get('/:userId', usersController.get); //own profile details
+userRoutes.put('/:userId', usersController.put); //change own profile details
+
+userRoutes.get('/:userId/search/books', usersController.searchBooks); // search all books for a user
+userRoutes.get('/:userId/search/books/:bookId', usersController.searchBookById); // search a book for a user
+
+userRoutes.get('/:userId/edit/books', usersController.userBooks); // own books list
+userRoutes.post('/:userId/edit/books', usersController.userBooks); // add a new owned book
+userRoutes.put('/:userId/edit/books', usersController.userBooks); // change details of an owned book
+userRoutes.delete('/:userId/edit/books', usersController.userBooks); // delete an owned book
+
+userRoutes.post('/:userId/request', usersController.userBooks); // request a book
 
 module.exports = userRoutes;
