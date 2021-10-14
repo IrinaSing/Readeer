173416ae255d7240b968018ed93067dd6d690be7
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-10-14 17:03:35 +0200

    books controller

---
+++ api/business-logic/books.js
@@ -0,0 +1,3 @@
+const booksManager = {};
+
+module.exports = booksManager;
--- api/controllers/books.js
+++ api/controllers/books.js
@@ -1,71 +1,36 @@
-const booksManager = require('../business-logic/books');
+const booksManager = require('../business-logic/books.js');
 
 const booksController = {
   get: async (req, res) => {
     try {
-      const allMessages = await booksManager.getAllMessages();
-      res.status(200).send(JSON.stringify(allMessages));
+      const allBooks = await booksManager.getAllBooks();
+      res.status(200).send(JSON.stringify(allBooks));
     } catch (error) {
       res.status(500).send(error);
     }
   },
-  getMessagesForChannel: async (req, res) => {
+  getBookById: async (req, res) => {
     try {
-      const channelId = req.params.channelId;
+      const bookId = req.params.bookId;
 
-      const messages = await booksManager.getMessagesForChannel(channelId);
+      const book = await booksManager.getBookById(bookId);
 
-      res.send(JSON.stringify(messages));
+      res.send(JSON.stringify(book));
     } catch (error) {
       res.status(500).send(error.message);
     }
   },
-  put: async (req, res) => {
+
+  search: async (req, res) => {
     try {
-      const messageId = req.params.messageId;
-
       const body = req.body;
-      const message = {
-        text: body.text,
-        id: body.id,
-        user: body.user,
-        date: body.date,
-        channelId: body.channelId,
-      };
 
-      if (body.id !== messageId) {
-        throw Error('Cannot change message ID after creation!');
+      if (body.filter) {
+        const books = await booksManager.searchBooks(body.filter);
+        res.status(200).send(JSON.stringify(books));
+      } else {
+        res.status(400).send('Bad request');
       }
-
-      const result = await booksManager.updateMessage(message);
-
-      res.status(200).send(JSON.stringify(result));
-    } catch (error) {
-      res.status(500).send(error.message);
-    }
-  },
-  post: async (req, res) => {
-    try {
-      const body = req.body;
-
-      const channelId = req.params.channelId;
-      const result = await booksManager.createMessage(
-        body.user,
-        body.text,
-        channelId
-      );
-      res.status(200).send(JSON.stringify(result));
-    } catch (error) {
-      res.status(500).send(error.message);
-    }
-  },
-  delete: async (req, res) => {
-    try {
-      const messageId = req.params.messageId;
-
-      const result = await booksManager.removeMessage(messageId);
-
-      res.status(200).send(JSON.stringify(result));
     } catch (error) {
       res.status(500).send(error.message);
     }
