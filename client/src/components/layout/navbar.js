e7a1d32cf310e3bbf13c1a49f1ad49f06f0bb218
sabanyelkenci <sabanyelkenci@gmail.com>
2021-10-31 12:01:13 +0100

    navbar fix

--- client/src/components/layout/navbar.js
+++ client/src/components/layout/navbar.js
@@ -1,28 +1,34 @@
+import deerIcon from "../../../public/icons/deer-icon-without-white.jpg";
+
 /**
  * The shared navbar.
  *
  * @param {object} routes - A routes object, see /src/routes.js.
  * @returns {HTMLDivElement} A rendered nav bar element.
  */
- export const navbar = (routes) => {
+export const navbar = (routes) => {
   const container = document.createElement("div");
-  container.className = "navbar-container";
-  const divLogo = document.createElement("div");
-  divLogo.className = "divLogo";
-  container.appendChild(divLogo);
-  const logoImg = document.createElement("img");
-  logoImg.className = "logo-image";
-  logoImg.src =
-    "https://image.shutterstock.com/image-vector/cute-deer-reading-book-adorable-260nw-1345878212.jpg";
-  divLogo.append(logoImg);
-  const logoText = document.createElement("p");
-  logoText.className = "logo-text";
-  logoText.innerHTML = "Readeer";
-  divLogo.append(logoText);
+  container.className = "container";
+  container.style.backgroundColor = "#1E78C7";
+  
 
-  const divNav = document.createElement("div");
-  divNav.className = "divNav";
-  container.appendChild(divNav);
+  // const container = document.createElement("div");
+  // container.className = "navbar-container";
+  // const divLogo = document.createElement("div");
+  // divLogo.className = "divLogo";
+  // container.appendChild(divLogo);
+  // const logoImg = document.createElement("img");
+  // logoImg.className = "logo-image";
+  // logoImg.src = deerIcon;
+  // divLogo.append(logoImg);
+  // const logoText = document.createElement("p");
+  // logoText.className = "logo-text";
+  // logoText.innerHTML = "Readeer";
+  // divLogo.append(logoText);
+
+  // const divNav = document.createElement("div");
+  // divNav.className = "divNav";
+  // container.appendChild(divNav);
 
   for (const route of routes) {
     const anchor = document.createElement("a");
