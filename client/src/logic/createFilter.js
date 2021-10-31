e08f779f488587e46f4c75c501f359cdcfbca14d
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-10-31 20:54:41 +0100

    working

--- client/src/logic/createFilter.js
+++ client/src/logic/createFilter.js
@@ -1,9 +1,9 @@
 export const createFilter = (userInput) => {
   const filter = {};
-
+  //TODO add to demonstration
   // "rose,author:Rosita,title:Rose and Daisy,language:en"
-
   //"author:Julia Gerigk"
+  //author:Rosita Steenbeek,title:Rose and Daisy
   const userSearchParams = userInput.split(',');
 
   for (let index = 0; index < userSearchParams.length; index++) {
