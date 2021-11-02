b905aee307e3f542e51588b3c14b0e1ffb00d95a
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-11-02 19:24:02 +0100

    a start

--- client/src/components/pages/home/index.js
+++ client/src/components/pages/home/index.js
@@ -1,18 +1,9 @@
-import { search } from './searchbar.js';
 import { goalsContent } from './goals.js';
 import { searchBarComponent } from '../../shared/searchbar.js';
 
 export const home = () => {
   const container = document.createElement('div');
   container.className = 'body';
-
-  container.appendChild(document.createTextNode('Welcome to HomePage!'));
-
-  container.appendChild(document.createElement('br'));
-
-  container.appendChild(search());
-  container.appendChild(document.createTextNode('Welcome to HomePage!'));
-  container.appendChild(document.createElement('br'));
 
   const searchBar = searchBarComponent();
   container.appendChild(searchBar);
