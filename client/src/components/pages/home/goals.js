e9fec735f5b72fb5b89133e47692f7d5aa0c62c4
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-11-02 21:08:10 +0100

    goal section background

--- client/src/components/pages/home/goals.js
+++ client/src/components/pages/home/goals.js
@@ -4,15 +4,15 @@ import natureCareImg from '../../../../public/icons/nature-care1.png';
 
 export const goalsContent = () => {
   const goalSection = document.createElement('div');
-  goalSection.classList = 'container home-goals';
+  goalSection.classList = 'container-fluid home-goals';
 
-  // const goalContainer = document.createElement('div');
-  // goalContainer.classList =
-  //   ' d-flex flex-column justify-content-center align-items-center ';
+  const goalSectionContainer = document.createElement('div');
+  goalSectionContainer.classList = 'container';
+  goalSection.appendChild(goalSectionContainer);
 
   const titleRow = document.createElement('div');
   titleRow.classList = 'row';
-  goalSection.appendChild(titleRow);
+  goalSectionContainer.appendChild(titleRow);
 
   const title = document.createElement('h1');
   title.classList = 'h1 mt-4 mb-3';
@@ -22,17 +22,15 @@ export const goalsContent = () => {
   content.innerText =
     'Readeer promotes reading and exchange of children’s books and can be used both by parents and children.';
 
-  // goalSection.appendChild(goalContainer);
-  // goalContainer.appendChild(title);
-  // goalContainer.appendChild(content);
   titleRow.appendChild(title);
   titleRow.appendChild(content);
 
   // Each sub section
   const subSections = document.createElement('div');
   subSections.classList = 'row';
-  goalSection.appendChild(subSections);
+  goalSectionContainer.appendChild(subSections);
 
+  // sub section 1
   const subSection1 = document.createElement('div');
   subSection1.classList = 'col-md-6';
   subSections.appendChild(subSection1);
@@ -62,8 +60,10 @@ export const goalsContent = () => {
   const contentSocial = document.createElement('p');
   contentSocial.classList = 'p_text lh-base my-5';
   contentSocial.innerText = `We think that using a book swap app is a nice way to involve children into sharing and exchanging activity as a part of socialization. 
-    Another goal is to make books more accessible: if you came from a different country, you may not be able to buy a book in your language or to find it in a library. But our app can help you. 
-    It is quite possible that you will find new acquaintances and friends for you and your child in your neighbourhood.`;
+    
+  Another goal is to make books more accessible: if you came from a different country, you may not be able to buy a book in your language or to find it in a library. But our app can help you. 
+    
+  It is quite possible that you will find new acquaintances and friends for you and your child in your neighbourhood.`;
 
   const socialContentCol = document.createElement('div');
   socialContentCol.classList = 'col-md-12';
@@ -71,7 +71,6 @@ export const goalsContent = () => {
   subSection1.appendChild(socialContentCol);
 
   // sub section 2
-
   const subSection2 = document.createElement('div');
   subSection2.classList = 'col-md-6';
   subSections.appendChild(subSection2);
@@ -109,7 +108,6 @@ export const goalsContent = () => {
   subSection2.appendChild(contentMoneyCol);
 
   //sub section 3
-
   const subSection3 = document.createElement('div');
   subSection3.classList = 'col-md-6';
   subSections.appendChild(subSection3);
@@ -129,7 +127,7 @@ export const goalsContent = () => {
 
   const titleEnv = document.createElement('h2');
   titleEnv.classList = 'h2 my-3';
-  titleEnv.innerText = 'Care for the environment';
+  titleEnv.innerText = 'Environmental care';
 
   const envTitleCol = document.createElement('div');
   envTitleCol.classList = 'col-md-8 d-flex align-items-center';
@@ -145,64 +143,6 @@ export const goalsContent = () => {
   envContentCol.classList = 'col-md-12';
   envContentCol.appendChild(contentEnv);
   subSection3.appendChild(envContentCol);
-
-  ///
-  // const goalsContainer = document.createElement('div');
-  // goalsContainer.classList = 'd-flex flex-row align-items-baseline';
-
-  // const goalSocial = document.createElement('div');
-  // goalSocial.classList = 'p-2';
-
-  // goalSocial.classList = 'social';
-
-  // goalContainer.appendChild(goalsContainer);
-  // goalsContainer.appendChild(goalSocial);
-  // goalSocial.appendChild(socialImg);
-  // goalSocial.appendChild(titleSocial);
-  // goalSocial.appendChild(contentSocial);
-
-  // const goalMoneyEnvi = document.createElement('div');
-  // goalMoneyEnvi.classList = 'p-2';
-  // goalMoneyEnvi.classList = 'moneyEnvi';
-
-  // const money = document.createElement('div');
-  // money.classList = 'p-2 bd-highlight';
-  // const moneyImg = document.createElement('img');
-  // moneyImg.src = logoCoins;
-  // moneyImg.style.maxWidth = '10%';
-  // const titleMoney = document.createElement('h2');
-  // titleMoney.classList = 'h2';
-  // titleMoney.innerText = 'Saving money';
-  // const contentMoney = document.createElement('p');
-  // contentMoney.classList = 'p_text';
-  // contentMoney.innerText =
-  //   'Books are getting more expensive, so exchange is a way to cooperate and help each other to save money. Instead of standing in the bookcase for years after reading, a book can bring joy to other children.';
-
-  // goalsContainer.appendChild(goalMoneyEnvi);
-  // goalMoneyEnvi.appendChild(money);
-  // money.appendChild(moneyImg);
-  // money.appendChild(titleMoney);
-  // money.appendChild(titleMoney);
-  // money.appendChild(contentMoney);
-
-  // const enviroment = document.createElement('div');
-  // enviroment.classList = 'p-2 bd-highlight';
-  // const natureImg = document.createElement('img');
-  // natureImg.src = natureCareImg;
-  // natureImg.style.maxWidth = '10%';
-  // const titleEnv = document.createElement('h2');
-  // titleEnv.classList = 'h2';
-  // titleEnv.innerText = 'Care for the environment';
-
-  // const contentEnv = document.createElement('p');
-  // contentEnv.classList = 'p_text';
-  // contentEnv.innerText =
-  //   'As you know, printing books requires cutting trees, which is not good for nature and climate. But also, books are completely reusable. Thus, book sharing is an ecological way to live.';
-
-  // goalMoneyEnvi.appendChild(enviroment);
-  // enviroment.appendChild(natureImg);
-  // enviroment.appendChild(titleEnv);
-  // enviroment.appendChild(contentEnv);
 
   return goalSection;
 };
