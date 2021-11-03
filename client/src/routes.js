d80d43aa9414657479fcf4ac4936ee0856320f27
IrinaSing <irinasingh.mail@yandex.ru>
2021-11-03 20:59:36 +0100

    table element

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,10 +1,10 @@
-import { page } from './components/layout/page.js';
+import { page } from "./components/layout/page.js";
 
-import { home } from './components/pages/home/index.js';
-import { register } from './components/pages/register/index.js';
-import { login } from './components/pages/login/index.js';
-import { about } from './components/pages/aboutUs/index.js';
-import { searchList } from './components/pages/searchList/index.js';
+import { home } from "./components/pages/home/index.js";
+import { register } from "./components/pages/register/index.js";
+import { login } from "./components/pages/login/index.js";
+import { about } from "./components/pages/aboutUs/index.js";
+import { searchList } from "./components/pages/search-list/index.js";
 
 /**
  * Defines the route URLs, names and callbacks.
@@ -13,21 +13,21 @@ import { searchList } from './components/pages/searchList/index.js';
  */
 
 const parse = (params = {}) => {
-  if (params === null || typeof params !== 'object') {
+  if (params === null || typeof params !== "object") {
     return {};
   }
   return Object.fromEntries(
     Object.entries(params)
-      .filter((param) => param[1] !== 'undefined')
+      .filter((param) => param[1] !== "undefined")
       .map((param) => [
         param[0],
-        param[1] !== '' && !Number.isNaN(Number(param[1]))
+        param[1] !== "" && !Number.isNaN(Number(param[1]))
           ? Number(param[1])
-          : param[1] === 'true'
+          : param[1] === "true"
           ? true
-          : param[1] === 'false'
+          : param[1] === "false"
           ? false
-          : param[1] === 'null'
+          : param[1] === "null"
           ? null
           : param[1],
       ])
@@ -41,30 +41,30 @@ const routeHandler =
   ({ params, data }) => {
     const namedRoutes = routes.filter((route) => `name` in route);
     const cleanData = { ...data };
-    cleanData.id = !data || !('id' in data) ? -1 : data.id;
-    const root = document.getElementById('root');
-    root.innerHTML = '';
+    cleanData.id = !data || !("id" in data) ? -1 : data.id;
+    const root = document.getElementById("root");
+    root.innerHTML = "";
     root.appendChild(page(pageBody(parse(data), parse(params)), namedRoutes));
   };
 
 [
   {
-    name: 'home',
+    name: "home",
     path: `/`,
     callback: routeHandler(home),
   },
   {
-    name: 'login',
+    name: "login",
     path: `/login`,
     callback: routeHandler(login),
   },
   {
-    name: 'register',
+    name: "register",
     path: `/register`,
     callback: routeHandler(register),
   },
   {
-    name: 'books',
+    name: "books",
     path: `/books`,
     callback: routeHandler(searchList),
   },
