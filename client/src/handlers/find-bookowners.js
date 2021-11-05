3d0ba40fa33f261211e362c99041ef44a8800440
IrinaSing <irinasingh.mail@yandex.ru>
2021-11-05 13:16:10 +0100

    alert if there are no offers for the book

--- client/src/handlers/find-bookowners.js
+++ client/src/handlers/find-bookowners.js
@@ -3,22 +3,26 @@
  *
  * @returns {Array} Array of objects with info about book offers.
  */
-import { createFilter } from '../logic/createFilter.js';
-import { state } from '../init/state.js';
-import { performBookSearchPost } from '../data-access/api-calls/calls.js';
-import { bookownersList } from '../components/pages/searchList/bookowners-list.js';
+import { createFilter } from "../logic/createFilter.js";
+import { state } from "../init/state.js";
+import { performBookSearchPost } from "../data-access/api-calls/calls.js";
+import { bookownersList } from "../components/pages/searchList/bookowners-list.js";
+import { errorAlert } from "../components/shared/error-alert.js";
 
 export const findBookOwners = (bookIsbn13) => {
-  const query = 'isbn_13:';
+  const query = "isbn_13:";
   const filterInput = query + bookIsbn13;
   const filter = createFilter(filterInput);
   state.searchFilter = filter;
 
   performBookSearchPost(state.searchFilter).then((books) => {
-    console.log(books);
-
-    const tableDiv = document.getElementById('listDiv');
-    const table = bookownersList(books);
-    tableDiv.appendChild(table);
+    const tableDiv = document.getElementById("listDiv");
+    if (books.length !== 0) {
+      const table = bookownersList(books);
+      tableDiv.appendChild(table);
+    } else {
+      const alert = errorAlert("There are no offers related to this book");
+      tableDiv.appendChild(alert);
+    }
   });
 };
