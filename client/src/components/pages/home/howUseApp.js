680c4c2c7e6028b19b5bf0e616b5bd7a21af7518
Sheyla <shel.busta@gmail.com>
2021-11-07 13:42:26 +0100

    component size

--- client/src/components/pages/home/howUseApp.js
+++ client/src/components/pages/home/howUseApp.js
@@ -3,7 +3,7 @@ export const howUseApp = () => {
   useAppSection.classList = "container fluid home-useapp";
 
   const useAppContainer = document.createElement("div");
-  useAppContainer.classList = "container";
+  useAppContainer.classList = "container my-5";
   useAppSection.appendChild(useAppContainer);
 
   const titleRow = document.createElement("div");
@@ -12,6 +12,8 @@ export const howUseApp = () => {
 
   const title = document.createElement("h1");
   title.classList = "h1 mt-4 mb-3";
+  title.style.color = "#1E78C7";
+  title.innerText = "How to use the app?";
 
   titleRow.appendChild(title);
 
@@ -29,10 +31,27 @@ export const howUseApp = () => {
 
   const contentColumn1 = document.createElement("p");
   contentColumn1.classList = "p_text lh-base my-5";
-  contentColumn1.innerText =
-    " To use the app you have to create an account. Add books that you want to exchange to your profile. Search for interesting books in your neighborhood. ";
+  contentColumn1.innerText = ` To use the app you have to create an account.
+    
+  Add books that you want to exchange to your profile. 
+  
+  Search for interesting books in your neighborhood.`;
+
+  subSection1.appendChild(contentColumn1);
 
   // sub section 2
+
+  const subSection2 = document.createElement("div");
+  subSection2.classList = "col-md-6";
+  subSections.appendChild(subSection2);
+
+  const contentColumn2 = document.createElement("p");
+  contentColumn2.classList = "p_text lh-base my-5";
+  contentColumn2.innerText = `Did you find something interesting? Offer a swap to the owner of the book or request to take it away!. 
+    
+  Contact the book owner to set up the swap meeting somewhere in the neighborhood.`;
+
+  subSection2.appendChild(contentColumn2);
 
   return useAppSection;
 };
