30ec2cff7462e70643d2baf51b70092e60cd4270
Anthony Meirlaen <anthony.meirlaen@gmail.com>
2021-11-07 14:38:19 +0100

    Add logout route, hide login and register when logged in

---
+++ client/src/handlers/do-logout.js
@@ -0,0 +1,7 @@
+import { router } from '../init/router.js';
+import { state } from '../init/state.js';
+
+export const doLogout = () => {
+    state.token = undefined;
+    router.navigate('/');
+};
