daf4c93e5b3934c18d05ade2c3de443b85290445
IrinaSing <irinasingh.mail@yandex.ru>
2021-11-08 23:52:13 +0100

    icons and headers section Goals

--- client/src/components/pages/home/goals.js
+++ client/src/components/pages/home/goals.js
@@ -1,146 +1,146 @@
-import socialBenefits from '../../../../public/icons/social-benefits.png';
-import logoCoins from '../../../../public/icons/coins.png';
-import natureCareImg from '../../../../public/icons/nature-care1.png';
+import socialBenefits from "../../../../public/icons/social-bg.png";
+import logoCoins from "../../../../public/icons/economy-bg.png";
+import natureCareImg from "../../../../public/icons/nature-care-bg.png";
 
 export const goalsContent = () => {
-  const goalSection = document.createElement('div');
-  goalSection.classList = 'container-fluid home-goals';
+  const goalSection = document.createElement("div");
+  goalSection.classList = "container-fluid home-goals";
 
-  const goalSectionContainer = document.createElement('div');
-  goalSectionContainer.classList = 'container';
+  const goalSectionContainer = document.createElement("div");
+  goalSectionContainer.classList = "container";
   goalSection.appendChild(goalSectionContainer);
 
-  const titleRow = document.createElement('div');
-  titleRow.classList = 'row';
+  const titleRow = document.createElement("div");
+  titleRow.classList = "row";
   goalSectionContainer.appendChild(titleRow);
 
-  const title = document.createElement('h1');
-  title.classList = 'h1 mt-4 mb-3';
-  const content = document.createElement('p');
-  content.classList = 'p_text mt-3 mb-4 lh-base';
-  title.innerText = 'Goals of the project';
+  const title = document.createElement("h1");
+  title.classList = "h1 mt-4 mb-3";
+  const content = document.createElement("p");
+  content.classList = "p_text mt-3 mb-4 lh-base";
+  title.innerText = "Goals of the project";
   content.innerText =
-    'Readeer promotes reading and exchange of children’s books and can be used both by parents and children.';
+    "Readeer promotes reading and exchange of children’s books and can be used both by parents and children.";
 
   titleRow.appendChild(title);
   titleRow.appendChild(content);
 
   // Each sub section
-  const subSections = document.createElement('div');
-  subSections.classList = 'row';
+  const subSections = document.createElement("div");
+  subSections.classList = "row";
   goalSectionContainer.appendChild(subSections);
 
   // sub section 1
-  const subSection1 = document.createElement('div');
-  subSection1.classList = 'col-md-6';
+  const subSection1 = document.createElement("div");
+  subSection1.classList = "col-md-6";
   subSections.appendChild(subSection1);
 
-  const headerSubsection1Row = document.createElement('div');
-  headerSubsection1Row.classList = 'row';
+  const headerSubsection1Row = document.createElement("div");
+  headerSubsection1Row.classList = "d-flex justify-content-start";
   subSection1.appendChild(headerSubsection1Row);
 
-  const socialImg = document.createElement('img');
+  const socialImg = document.createElement("img");
   socialImg.src = socialBenefits;
-  socialImg.style.width = '100%';
+  socialImg.style.width = "50%";
 
-  const imgcol1 = document.createElement('div');
-  imgcol1.classList = 'col-md-4';
+  const imgcol1 = document.createElement("div");
+  imgcol1.classList = "w-10 mx-auto my-auto";
   imgcol1.appendChild(socialImg);
   headerSubsection1Row.appendChild(imgcol1);
 
-  const titleSocial = document.createElement('h2');
-  titleSocial.classList = 'h2 my-3';
-  titleSocial.innerText = 'Social benefits';
+  const titleSocial = document.createElement("h2");
+  titleSocial.classList = "my-3";
+  titleSocial.innerText = "Social benefits";
 
-  const socialTitleCol = document.createElement('div');
-  socialTitleCol.classList = 'col-md-8 d-flex align-items-center';
+  const socialTitleCol = document.createElement("div");
+  // socialTitleCol.classList = "d-flex justify-content-center";
   socialTitleCol.appendChild(titleSocial);
   headerSubsection1Row.appendChild(socialTitleCol);
 
-  const contentSocial = document.createElement('p');
-  contentSocial.classList = 'p_text lh-base my-5';
+  const contentSocial = document.createElement("p");
+  contentSocial.classList = "p_text lh-base my-5";
   contentSocial.innerText = `We think that using a book swap app is a nice way to involve children into sharing and exchanging activity as a part of socialization. 
     
   Another goal is to make books more accessible: if you came from a different country, you may not be able to buy a book in your language or to find it in a library. But our app can help you. 
     
   It is quite possible that you will find new acquaintances and friends for you and your child in your neighbourhood.`;
 
-  const socialContentCol = document.createElement('div');
-  socialContentCol.classList = 'col-md-12';
+  const socialContentCol = document.createElement("div");
+  socialContentCol.classList = "col-md-12";
   socialContentCol.appendChild(contentSocial);
   subSection1.appendChild(socialContentCol);
 
   // sub section 2
-  const subSection2 = document.createElement('div');
-  subSection2.classList = 'col-md-6';
+  const subSection2 = document.createElement("div");
+  subSection2.classList = "col-md-6";
   subSections.appendChild(subSection2);
 
-  const headerSubsection2Row = document.createElement('div');
-  headerSubsection2Row.classList = 'row';
+  const headerSubsection2Row = document.createElement("div");
+  headerSubsection2Row.classList = "d-flex justify-content-start";
   subSection2.appendChild(headerSubsection2Row);
 
-  const moneyImg = document.createElement('img');
+  const moneyImg = document.createElement("img");
   moneyImg.src = logoCoins;
-  moneyImg.style.width = '100%';
+  moneyImg.style.width = "50%";
 
-  const imgcol2 = document.createElement('div');
-  imgcol2.classList = 'col-md-4';
+  const imgcol2 = document.createElement("div");
+  imgcol2.classList = "w-10 mx-auto my-auto";
   imgcol2.appendChild(moneyImg);
   headerSubsection2Row.appendChild(imgcol2);
 
-  const titleMoney = document.createElement('h2');
-  titleMoney.classList = 'h2 my-3';
-  titleMoney.innerText = 'Saving money';
+  const titleMoney = document.createElement("h2");
+  titleMoney.classList = "my-3";
+  titleMoney.innerText = "Saving money";
 
-  const moneyTitleCol = document.createElement('div');
-  moneyTitleCol.classList = 'col-md-8 d-flex align-items-center';
+  const moneyTitleCol = document.createElement("div");
+  moneyTitleCol.classList = "col-md-8 d-flex align-items-center";
   moneyTitleCol.appendChild(titleMoney);
   headerSubsection2Row.appendChild(moneyTitleCol);
 
-  const contentMoney = document.createElement('p');
-  contentMoney.classList = 'p_text lh-base my-5';
+  const contentMoney = document.createElement("p");
+  contentMoney.classList = "p_text lh-base my-5";
   contentMoney.innerText =
-    'Books are getting more expensive, so exchange is a way to cooperate and help each other to save money. Instead of standing in the bookcase for years after reading, a book can bring joy to other children.';
+    "Books are getting more expensive, so exchange is a way to cooperate and help each other to save money. Instead of standing in the bookcase for years after reading, a book can bring joy to other children.";
 
-  const contentMoneyCol = document.createElement('div');
-  contentMoneyCol.classList = 'col-md-12';
+  const contentMoneyCol = document.createElement("div");
+  contentMoneyCol.classList = "col-md-12";
   contentMoneyCol.appendChild(contentMoney);
   subSection2.appendChild(contentMoneyCol);
 
   //sub section 3
-  const subSection3 = document.createElement('div');
-  subSection3.classList = 'col-md-6';
+  const subSection3 = document.createElement("div");
+  subSection3.classList = "col-md-6";
   subSections.appendChild(subSection3);
 
-  const headerSubsection3Row = document.createElement('div');
-  headerSubsection3Row.classList = 'row';
+  const headerSubsection3Row = document.createElement("div");
+  headerSubsection3Row.classList = "d-flex justify-content-start";
   subSection3.appendChild(headerSubsection3Row);
 
-  const natureImg = document.createElement('img');
+  const natureImg = document.createElement("img");
   natureImg.src = natureCareImg;
-  natureImg.style.width = '100%';
+  natureImg.style.width = "50%";
 
-  const imgcol3 = document.createElement('div');
-  imgcol3.classList = 'col-md-4';
+  const imgcol3 = document.createElement("div");
+  imgcol3.classList = "w-10 mx-auto my-auto";
   imgcol3.appendChild(natureImg);
   headerSubsection3Row.appendChild(imgcol3);
 
-  const titleEnv = document.createElement('h2');
-  titleEnv.classList = 'h2 my-3';
-  titleEnv.innerText = 'Environmental care';
+  const titleEnv = document.createElement("h2");
+  titleEnv.classList = "my-3";
+  titleEnv.innerText = "Care for planet";
 
-  const envTitleCol = document.createElement('div');
-  envTitleCol.classList = 'col-md-8 d-flex align-items-center';
+  const envTitleCol = document.createElement("div");
+  envTitleCol.classList = "col-md-8 d-flex align-items-center";
   envTitleCol.appendChild(titleEnv);
   headerSubsection3Row.appendChild(envTitleCol);
 
-  const contentEnv = document.createElement('p');
-  contentEnv.classList = 'p_text lh-base my-5';
+  const contentEnv = document.createElement("p");
+  contentEnv.classList = "p_text lh-base my-5";
   contentEnv.innerText =
-    'As you know, printing books requires cutting trees, which is not good for nature and climate. But also, books are completely reusable. Thus, book sharing is an ecological way to live.';
+    "As you know, printing books requires cutting trees, which is not good for nature and climate. But also, books are completely reusable. Thus, book sharing is an ecological way to live.";
 
-  const envContentCol = document.createElement('div');
-  envContentCol.classList = 'col-md-12';
+  const envContentCol = document.createElement("div");
+  envContentCol.classList = "col-md-12";
   envContentCol.appendChild(contentEnv);
   subSection3.appendChild(envContentCol);
 
