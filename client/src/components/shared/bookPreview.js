3c18af213f31193ef31371b688652d3c4d69148d
IrinaSing <irinasingh.mail@yandex.ru>
2021-11-13 20:30:03 +0100

    changes

--- client/src/components/shared/bookPreview.js
+++ client/src/components/shared/bookPreview.js
@@ -1,43 +1,43 @@
 /* eslint-disable filenames/match-regex */
-import { googleManager } from '../../business-logic/googleBooksAPI.js';
-import classes from './bookPreview.module.css';
+import { googleManager } from "../../business-logic/googleBooksAPI.js";
+import classes from "./bookPreview.module.css";
 
 export const bookPreview = (
   id,
-  title = 'Unknown',
-  description = '',
-  isbn_10 = '',
-  isbn_13 = '',
+  title = "Unknown",
+  description = "",
+  isbn_10 = "",
+  isbn_13 = "",
 
   onClick = (id) => {}
 ) => {
-  const container = document.createElement('article');
+  const container = document.createElement("article");
   container.classList.add(classes.preview);
 
-  const image = document.createElement('img');
-  image.alt = 'book cover';
-  image.style.visibility = 'hidden';
+  const image = document.createElement("img");
+  image.alt = "book cover";
+  image.style.visibility = "hidden";
 
   // fetch the image from the google books api
   googleManager.getPictureURL(isbn_10).then((url) => {
     image.src = url;
-    image.style.visibility = 'visible';
+    image.style.visibility = "visible";
   });
 
-  const header = document.createElement('h1');
+  const header = document.createElement("h1");
   header.innerText = title;
 
-  const isbn = document.createElement('p');
+  const isbn = document.createElement("p");
   isbn.innerText = `${isbn_10} - ${isbn_13}`;
 
-  const descriptor = document.createElement('p');
+  const descriptor = document.createElement("p");
   descriptor.innerText = description;
   descriptor.classList.add(classes.descriptor);
 
-  const viewListings = document.createElement('a');
-  viewListings.innerText = 'View';
-  viewListings.href = '#';
-  viewListings.addEventListener('click', (event) => {
+  const viewListings = document.createElement("a");
+  viewListings.innerText = "View";
+  viewListings.href = "#";
+  viewListings.addEventListener("click", (event) => {
     event.preventDefault();
     onClick(id);
   });
