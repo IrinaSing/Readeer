a45b4813576c8f21dd58b15b7f50f4ff6ffa51ec
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-11-14 09:45:01 +0100

    renaming and empty div removal

--- client/src/components/pages/home/index.js
+++ client/src/components/pages/home/index.js
@@ -1,13 +1,13 @@
-import { goalsContent } from "./goals.js";
-import { searchBarComponent } from "../../shared/searchbar.js";
-import { showcase } from "./sections";
-import { useful } from "./sections";
-import { howUseApp } from "./howUseApp.js";
-import { videoSection } from "./Video.js";
+import { goalsContent } from './goals.js';
+import { searchBarComponent } from '../../shared/searchbar.js';
+import { showcase } from './sections';
+import { useful } from './sections';
+import { howUseApp } from './howUseApp.js';
+import { videoSection } from './videoPlayer.js';
 
 export const home = () => {
-  const container = document.createElement("div");
-  container.className = "body";
+  const container = document.createElement('div');
+  container.className = 'body';
 
   const searchBar = searchBarComponent();
   container.appendChild(searchBar);
