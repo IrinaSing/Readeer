053b4fddc8a6293cd51729caced90999708e6e8e
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-11-16 19:54:46 +0100

    partial footer

--- client/src/components/layout/footer.js
+++ client/src/components/layout/footer.js
@@ -1,134 +1,100 @@
-import emailIcon from "../../../public/email-icon.svg";
-import githubIcon from "../../../public/git-white.svg";
-import hyfLogo from "../../../public/hyf-logo.svg";
+import emailIcon from '../../../public/email-icon.svg';
+import githubIcon from '../../../public/git-white.svg';
+import hyfLogo from '../../../public/hyf-logo.svg';
 /**
  * The shared footer.
  *
  * @returns {HTMLDivElement} A rendered footer element.
  */
 export const footer = () => {
-  const containerFooter = document.createElement("footer");
-  containerFooter.classList = "page-footer font-small mdb-color pt-4 ";
-  containerFooter.style.paddingLeft = "0px";
+  const containerFooter = document.createElement('footer');
+  containerFooter.classList =
+    'page-footer font-small mdb-color p-4 bg-primary text-white';
+  containerFooter.style.paddingLeft = '0px';
 
   // container
 
-  const container = document.createElement("div");
-  container.classList = "container-fluid  footerr ";
+  const container = document.createElement('div');
+  container.classList = 'container-fluid';
 
   containerFooter.appendChild(container);
 
-  // sub containers
+  const row = document.createElement('div');
+  row.classList = 'row';
+  container.appendChild(row);
 
-  const subContainers = document.createElement("div");
-  subContainers.classList = "row py-4";
-  container.appendChild(subContainers);
+  // col-md-4
+  const colEmail = document.createElement('div');
+  colEmail.classList = 'col-md-4';
+  row.appendChild(colEmail);
 
-  // Email contact
+  const centerDiv1 = document.createElement('div');
+  centerDiv1.classList = 'mx-auto ';
+  colEmail.appendChild(centerDiv1);
 
-  const emailContact = document.createElement("div");
-  emailContact.className = "col-md-3 col-lg-3 col-xl-3 mx-auto my-3";
+  const imgDiv1 = document.createElement('div');
+  imgDiv1.classList = 'rounded-circle mx-auto d-flex justify-content-center';
+  centerDiv1.appendChild(imgDiv1);
 
-  const emailRow = document.createElement("div");
-  emailRow.classList = "row";
-  emailContact.appendChild(emailRow);
-
-  const emailImg = document.createElement("img");
+  const emailImg = document.createElement('img');
   emailImg.src = emailIcon;
+  imgDiv1.appendChild(emailImg);
 
-  subContainers.appendChild(emailContact);
-  emailContact.appendChild(emailImg);
+  const contentDiv1 = document.createElement('div');
+  contentDiv1.classList = 'text-center';
+  centerDiv1.appendChild(contentDiv1);
 
-  const imgcol1 = document.createElement("div");
-  imgcol1.classList = "col-auto my-auto";
-  imgcol1.appendChild(emailImg);
-  emailRow.appendChild(imgcol1);
+  const contactUs = document.createElement('p');
+  contactUs.classList = 'pt-2 mb-0';
+  contactUs.innerHTML = `Contact us
+  <a href="mailto:readeer-app@gmail.com" style="text-decoration: none; color: white;"></a>`;
+  contentDiv1.appendChild(contactUs);
 
-  const contentCol1 = document.createElement("div");
-  contentCol1.classList = "col-auto my-auto";
-  const contactUs = document.createElement("p");
-  contactUs.classList = "mb-0";
-  contactUs.innerText = `Contact us
-  readeer@ml.com`;
-  contentCol1.appendChild(contactUs);
-  emailContact.appendChild(contentCol1);
-  emailRow.appendChild(contentCol1);
+  const contactLink = document.createElement('a');
+  contactLink.href = 'mailto:readeer-app@gmail.com';
+  // contactLink.classList = 'text-center';
+  contactLink.innerText = 'readeer@ml.com';
+  contactLink.style.color = 'white';
+  contactLink.style.textDecoration = 'none';
+  contentDiv1.appendChild(contactLink);
 
-  const mailTo = document.createElement("a");
-  mailTo.href = "mailto:readeer-app@gmail.com";
-  mailTo.style.textDecoration = "none";
-  mailTo.style.color = "white";
-  mailTo.appendChild(emailRow);
-  emailContact.appendChild(mailTo);
+  //col-md-4
+  const colSource = document.createElement('div');
+  colSource.classList = 'col-md-4';
+  row.appendChild(colSource);
 
-  //  GitHub and Copyright
+  const centerDiv2 = document.createElement('div');
+  centerDiv2.classList = 'mx-auto d-flex justify-content-center';
+  colSource.appendChild(centerDiv2);
 
-  const gitCopyRight = document.createElement("div");
-  gitCopyRight.className = "col-md-3 col-lg-3 col-xl-3 mx-auto my-3";
-
-  const gitCopyRightRow = document.createElement("div");
-  gitCopyRightRow.classList = "row";
-  gitCopyRight.appendChild(gitCopyRightRow);
-
-  const gitHubImg = document.createElement("img");
+  const gitHubImg = document.createElement('img');
   gitHubImg.src = githubIcon;
+  centerDiv2.appendChild(gitHubImg);
 
-  subContainers.appendChild(gitCopyRight);
+  const content = document.createElement('p');
+  content.classList = 'p-3 mb-0 d-block';
+  content.innerHTML = `Source code
+    <a href="https://github.com/IrinaSing/Readeer" style="text-decoration: none; color: white;">© 2021 Readeer</a>`;
+  centerDiv2.appendChild(content);
 
-  const imgcol2 = document.createElement("div");
-  imgcol2.classList = "col-auto my-auto";
-  imgcol2.appendChild(gitHubImg);
-  gitCopyRightRow.appendChild(imgcol2);
+  //col-md-4
+  const colLogo = document.createElement('div');
+  colLogo.classList = 'col-md-4';
+  row.appendChild(colLogo);
 
-  const contentCol2 = document.createElement("div");
-  contentCol2.classList = "col-auto my-auto";
-  const content = document.createElement("p");
-  content.classList = "mb-0";
-  content.innerText = `Source code
-    © 2021 Readeer`;
-  contentCol2.appendChild(content);
-  gitCopyRightRow.appendChild(contentCol2);
+  const centerDiv3 = document.createElement('div');
+  centerDiv3.classList = 'mx-auto d-flex justify-content-center';
+  colLogo.appendChild(centerDiv3);
 
-  const gitLink = document.createElement("a");
-  gitLink.href = "https://github.com/IrinaSing/Readeer";
-  gitLink.style.textDecoration = "none";
-  gitLink.style.color = "white";
-  gitLink.appendChild(gitCopyRightRow);
-  gitCopyRight.appendChild(gitLink);
-
-  // Hack your future
-
-  const hackYourFuture = document.createElement("div");
-  hackYourFuture.className = "col-md-3 col-lg-3 col-xl-3 mx-auto my-3";
-  subContainers.appendChild(hackYourFuture);
-
-  const hackYourFutureRow = document.createElement("div");
-  hackYourFutureRow.classList = "row";
-
-  const hyfImg = document.createElement("img");
+  const hyfImg = document.createElement('img');
   hyfImg.src = hyfLogo;
+  centerDiv3.appendChild(hyfImg);
 
-  const imgcol3 = document.createElement("div");
-  imgcol3.classList = "col-auto my-auto";
-  imgcol3.appendChild(hyfImg);
-  hackYourFutureRow.appendChild(imgcol3);
-
-  const contentCol3 = document.createElement("div");
-  contentCol3.classList = "col-auto my-auto";
-  const hyfContent = document.createElement("p");
-  hyfContent.classList = "mb-0";
-  hyfContent.innerText = `Final project
-  for HYF Belgium`;
-  contentCol3.appendChild(hyfContent);
-
-  const hyfLink = document.createElement("a");
-  hyfLink.href = "https://hackyourfuture.be/";
-  hyfLink.style.textDecoration = "none";
-  hyfLink.style.color = "white";
-
-  hackYourFutureRow.appendChild(contentCol3);
-  hyfLink.appendChild(hackYourFutureRow);
-  hackYourFuture.appendChild(hyfLink);
+  const hyfContent = document.createElement('p');
+  hyfContent.classList = 'p-3 mb-0';
+  hyfContent.innerHTML = `Final project
+    for <a href="https://hackyourfuture.be/" style="text-decoration: none; color: white;">HYF Belgium</a>`;
+  centerDiv3.appendChild(hyfContent);
 
   return containerFooter;
 };
